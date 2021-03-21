5adfb9763efdfd34c866ae9451b79d5b54bf5768
Julia Kostina <70097357+julsunny@users.noreply.github.com>
2021-03-21 13:55:19 +0100

    Update read_audio.py

--- read_audio.py
+++ read_audio.py
@@ -3,3 +3,20 @@ import pyaudio
 import numpy as np
 import config
 
+
+p = pyaudio.PyAudio()
+    frames_per_buffer = int(config.MIC_RATE / config.FPS)
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=config.MIC_RATE,
+                    input=True,
+                    frames_per_buffer=frames_per_buffer)
+    
+
+while True:
+    if stream.is_stopped():
+        stream.start_stream() 
+    buf = stream.read(1024)
+    if buf:
+        stream.stop_stream()
+        decoder.process_raw(buf, False, False)
