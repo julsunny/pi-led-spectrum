cc152f75df9f19b6703e725e7782b6d1b135bf1a
Julia Kostina <70097357+julsunny@users.noreply.github.com>
2021-03-21 15:28:17 +0100

    Update read_audio.py

--- read_audio.py
+++ read_audio.py
@@ -4,18 +4,28 @@ import numpy as np
 import config
 
 
-p = pyaudio.PyAudio()
-frames_per_buffer = int(config.MIC_RATE / config.FPS)
-stream = p.open(format=pyaudio.paInt16,
+def start_stream(callback):
+    p = pyaudio.PyAudio()
+    frames_per_buffer = int(config.MIC_RATE / config.FPS)
+    stream = p.open(format=pyaudio.paInt16,
                     channels=1,
                     rate=config.MIC_RATE,
+                    input_device_index = config.DEV_INDEX,
                     input=True,
                     frames_per_buffer=frames_per_buffer)
-    
-
-while True:
-    if stream.is_stopped():
-        stream.start_stream() 
-    buf = stream.read(1024)
-    if buf:
-        stream.stop_stream()
+    overflows = 0
+    prev_ovf_time = time.time()
+    while True:
+        try:
+            y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
+            y = y.astype(np.float32)
+            stream.read(stream.get_read_available(), exception_on_overflow=False)
+            callback(y)
+        except IOError:
+            overflows += 1
+            if time.time() > prev_ovf_time + 1:
+                prev_ovf_time = time.time()
+                print('Audio buffer has overflowed {} times'.format(overflows))
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
